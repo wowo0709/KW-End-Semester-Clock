6a57540b1343dcfb242a61bd8c31ce5fdb68d927
wowo0709 <wjsduddn0709@naver.com>
2021-11-01 23:05:29 +0900

    NoticeActivity: upload tablayout, fragments and connect

--- EndSemesterClock/app/src/main/java/kr/co/kw_seniors/endsemesterclock/MainActivity.kt
+++ EndSemesterClock/app/src/main/java/kr/co/kw_seniors/endsemesterclock/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.kw_seniors.endsemesterclock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.co.kw_seniors.endsemesterclock.databinding.ActivityMainBinding
@@ -11,6 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // 공지 버튼 리스너
+        binding.btnNotice.setOnClickListener {
+            val intent = Intent(this, NoticeActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
