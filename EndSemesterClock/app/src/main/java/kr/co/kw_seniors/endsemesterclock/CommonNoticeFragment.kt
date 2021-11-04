c69e7f7a1809331a881e663e751b33cee243e917
wowo0709 <wjsduddn0709@naver.com>
2021-11-04 21:20:16 +0900

    update Notice - recyclerview, adapter, item

--- EndSemesterClock/app/src/main/java/kr/co/kw_seniors/endsemesterclock/CommonNoticeFragment.kt
+++ EndSemesterClock/app/src/main/java/kr/co/kw_seniors/endsemesterclock/CommonNoticeFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kr.co.kw_seniors.endsemesterclock.databinding.FragmentCommonNoticeBinding
 
 
 class CommonNoticeFragment : Fragment() {
+
+    lateinit var binding: FragmentCommonNoticeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_common_notice, container, false)
+        binding = FragmentCommonNoticeBinding.inflate(inflater, container, false)
+
+        val data = loadData()
+
+        var adapter = NoticeRecyclerAdapter()
+        adapter.listData = data
+        binding.recyclerViewCommon.adapter = adapter
+        binding.recyclerViewCommon.layoutManager = LinearLayoutManager(this.context)
+
+
+
+        return binding.root
+    }
+
+    // Room에 저장된 아이템 리스트 불러오기  
+    fun loadData(): MutableList<NoticeRecyclerItem>{
+        val data: MutableList<NoticeRecyclerItem> = mutableListOf()
+
+        // Room의 데이터 가져오기
+
+
+
+        return data
     }
 
 }
